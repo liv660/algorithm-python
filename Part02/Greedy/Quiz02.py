c753fd2fec9fa12e339612f8d1c6322706f80777
sysim <liv660n@gmail.com>
2022-03-19 15:19:26 +0900

    Q2.큰 수의 법칙

---
+++ Part02/Greedy/Quiz02.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+N, M, K = map(int, input().split())
+# print("N M K :", N, M, K)
+
+data = list(map(int, input().split()))
+# print("data:", data)
+
+data.sort(reverse = True)
+# print("내림차순:", data)
+
+i = 0
+flag = 0
+result = 0
+
+while i < M:
+  if (flag < K):
+    result += data[0]
+    flag += 1
+  else:
+    result += data[1]
+    flag = 0
+  i += 1
+
+print("큰 수:", result)
