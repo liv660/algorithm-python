6c4df3eb5bd819aeec487d171fed69b0927ee86b
sysim <liv660n@gmail.com>
2022-03-20 18:14:50 +0900

    Q2.큰 수의 법칙

--- Part02/Greedy/Quiz02.py
+++ Part02/Greedy/Quiz02.py
@@ -1,25 +1,22 @@
 # p.92 실전문제 02 - 큰 수의 법칙
 
+# 1. 입력
 N, M, K = map(int, input().split())
-# print("N M K :", N, M, K)
-
 data = list(map(int, input().split()))
-# print("data:", data)
 
+# 2. 내림차순 정렬
 data.sort(reverse = True)
-# print("내림차순:", data)
 
-i = 0
-flag = 0
-result = 0
+# print("count\t val")
+# 3. 연속 K번, M번 더하기
+cnt = 0
+val = 0
+while cnt < M:
+    for i in range(K):
+        val += data[0]
 
-while i < M:
-  if (flag < K):
-    result += data[0]
-    flag += 1
-  else:
-    result += data[1]
-    flag = 0
-  i += 1
+    val += data[1]
+    cnt += (K+1)
+    # print(count,"\t\t",val)
 
-print("큰 수:", result)
+print(val)
