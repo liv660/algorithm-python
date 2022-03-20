c53a65f3ed172d60da4cee7512aa03c3e89d21d2
sysim <liv660n@gmail.com>
2022-03-20 12:45:25 +0900

    Q4.미로 탈출

---
+++ Part02/DFS_BFS/Quiz04.py
@@ -0,0 +1,34 @@
+# p.152 실전문제 04 - 미로탈출
+
+# CASE: N = 5, M = 6
+# 1 0 1 0 1 0
+# 1 1 1 1 1 1
+# 0 0 0 0 0 1
+# 1 1 1 1 1 1
+# 1 1 1 1 1 1
+
+N, M = map(int, input().split())
+# print("세로:", N, "\t가로:", M)
+
+# 1. 2차원 배열로 입력 저장
+maze = []
+
+for i in range(N):
+    maze.append(list(map(int, input().split())))
+# print(maze)
+# print("x\t y\t count")
+
+# 2. 미로 탈출
+def escape(x,y,cnt):
+    # print(x,"\t",y,"\t",cnt)
+    if(x == N-1 and y == M-1): return cnt # 좌표가 출구이면 탈출!
+    if(y == M-1 and x < N): escape(x+1, y, cnt+1) # 가로 마지막 인덱스일 때 아래로 좌표 이동
+    if(x == N-1 and x < M): escape(x, y+1, cnt+1) # 세로 마지막 인덱스일 때 오른쪽으로 좌표 이동
+
+    if(maze[x][y+1] == 0):
+        escape(x+1, y, cnt+1)
+    else:
+        escape(x,y+1,cnt+1)
+
+result = escape(0, 0, 1)
+print("result:", result)
